299bdef8903c52f45205f3f90e9a0fec82836a18
aayushisolanki28 <36412316+aayushisolanki28@users.noreply.github.com>
2023-06-19 13:55:58 +0530

    5 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./style.css";
+import { v4 as uuid } from "uuid";
+import React, { useState, useEffect } from "react";
 import Header from './components/Header'
 import AddContact from './components/AddContact'
-export default function App() {
+import ContactList from './components/ContactList'
+// import ContactCard from './components/ContactCard'
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  // useEffect(() => {
+  //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (retriveContacts) setContacts(retriveContacts);
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div>
-      <Header/>
-      <AddContact/>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
+
+export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactCard = ()=>{
+  return(
+    <div className ="item">
+    <div className ="content">
+   <div className ="header">{e.name}
+   </div>
+   </div>
+   <div className ="content">
+   <div className ="header">{e.email}
+   </div>
+   </div>
+   <i className ="trash alternate outline icon" style ={{marginLeft : 100,color :red}}/>
+  </div>
+  )
+}
+export default ContactCard
